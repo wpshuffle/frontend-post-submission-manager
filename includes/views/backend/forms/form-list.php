0638e18c88423def794eab1bb0417417fc5fe3ba
Regan Khadgi <regan.khadgi1@gmail.com>
2019-05-26 13:17:44 +0100

    activation classed moved inside admin

--- includes/views/backend/forms/form-list.php
+++ includes/views/backend/forms/form-list.php
@@ -1,9 +1,12 @@
 <?php
-defined( 'ABSPATH' ) or die( 'No script kiddies please!!' );
+defined('ABSPATH') or die('No script kiddies please!!');
 ?>
 <div class="wrap fpsm-wrap">
     <div class="fpsm-header fpsm-clearfix">
-        <h1 class="fpsm-floatLeft"><?php _e( 'Frontend Post Submission Manager', 'frontend-post-submission-manager' ); ?></h1>
-        <h2 class="fpsm-floatRight"><?php _e( 'Form Lists', 'frontend-post-submission-manager' ); ?></h2>
+        <h1 class="fpsm-floatLeft"><?php _e('Frontend Post Submission Manager', 'frontend-post-submission-manager'); ?></h1>
+        <h2 class="fpsm-floatRight"><?php _e('Form Lists', 'frontend-post-submission-manager'); ?></h2>
+    </div>
+    <div class="fpsm-form-add-wrap">
+        <a href="<?php echo admin_url('admin.php?page=fpsm&action=add-form'); ?>"></a>
     </div>
 </div>
