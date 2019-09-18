768b66f49bd1cead508609e0b332dcaa3898bbc8
Regan Khadgi <regan.khadgi1@gmail.com>
2019-09-18 12:05:11 +0100

    admin menu added

--- includes/classes/admin/class-fpsm-admin.php
+++ includes/classes/admin/class-fpsm-admin.php
@@ -11,9 +11,17 @@ if (!class_exists('FPSM_Admin')) {
         }
 
         function add_admin_menus() {
+            if (!empty($_GET['action']) && $_GET['action'] == 'edit_form') {
+                $page_title = esc_html__('Edit Form', 'frontend-post-submission-manager');
+            } else {
+                $page_title = esc_html__('All forms', 'frontend-post-submission-manager');
+            }
             add_menu_page(esc_html__('Frontend Post Submission', 'frontend-post-submission-manager'), esc_html__('Frontend Post Submission', 'frontend-post-submission-manager'), 'manage_options', 'fpsm', array($this, 'form_lists'), 'dashicons-format-aside');
-            add_submenu_page('fpsm', esc_html__('All Forms', 'frontend-post-submission-manager'), esc_html__('All Forms', 'frontend-post-submission-manager'), 'manage_options', 'fpsm', array($this, 'form_lists'));
+            add_submenu_page('fpsm', $page_title, esc_html__('All Forms', 'frontend-post-submission-manager'), 'manage_options', 'fpsm', array($this, 'form_lists'));
             add_submenu_page('fpsm', esc_html__('Add New Form', 'frontend-post-submission-manager'), esc_html__('Add New Form', 'frontend-post-submission-manager'), 'manage_options', 'fpsm-add-new-form', array($this, 'form_adder'));
+            add_submenu_page('fpsm', esc_html__('Setting', 'frontend-post-submission-manager'), esc_html__('Settings', 'frontend-post-submission-manager'), 'manage_options', 'fpsm-settings', array($this, 'render_form_settings_page'));
+            add_submenu_page('fpsm', esc_html__('Help', 'frontend-post-submission-manager'), esc_html__('Help', 'frontend-post-submission-manager'), 'manage_options', 'fpsm-help', array($this, 'render_form_help_page'));
+            add_submenu_page('fpsm', esc_html__('About', 'frontend-post-submission-manager'), esc_html__('About', 'frontend-post-submission-manager'), 'manage_options', 'fpsm-about', array($this, 'render_form_about_page'));
         }
 
         function form_lists() {
@@ -31,6 +39,18 @@ if (!class_exists('FPSM_Admin')) {
 
         function form_adder() {
             include(FPSM_PATH . '/includes/views/backend/forms/form-add.php');
+        }
+
+        function render_form_settings_page() {
+            include(FPSM_PATH . '/includes/views/backend/settings.php');
+        }
+
+        function render_form_help_page() {
+            include(FPSM_PATH . '/includes/views/backend/help.php');
+        }
+
+        function render_form_about_page() {
+            include(FPSM_PATH . '/includes/views/backend/about.php');
         }
 
         function add_extra_html() {
