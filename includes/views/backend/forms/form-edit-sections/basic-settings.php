73ccf3036e0b22af552ac63f324e650db8550576
Regan Khadgi <regan.khadgi1@gmail.com>
2019-09-18 10:46:32 +0100

    backend edit changes

--- includes/views/backend/forms/form-edit-sections/basic-settings.php
+++ includes/views/backend/forms/form-edit-sections/basic-settings.php
@@ -1,32 +1,38 @@
+<?php
+$form_details = (!empty($form_row->form_details)) ? $form_row->form_details : '';
+$form_details = maybe_unserialize($form_details);
+$basic_settings = (!empty($form_details['basic'])) ? $form_details['basic'] : array();
+?>
 <div class="fpsm-settings-each-section" data-tab="basic">
     <div class="fpsm-field-wrap">
         <label><?php esc_html_e('Form Status', 'frontend-post-submission-manager'); ?></label>
         <div class="fpsm-field">
-            <input type="checkbox" name="form_status" value="1"/>
+            <input type="checkbox" name="form_status" value="1" <?php echo (!empty($form_row->form_status)) ? 'checked="checked"' : ''; ?>/>
         </div>
     </div>
     <div class="fpsm-field-wrap">
         <label><?php esc_html_e('Form Title', 'frontend-post-submission-manager'); ?></label>
         <div class="fpsm-field">
-            <input type="text" name="form_title"/>
+            <input type="text" name="form_title" value="<?php echo esc_attr($form_row->form_title); ?>"/>
         </div>
     </div>
     <div class="fpsm-field-wrap">
         <label><?php esc_html_e('Form Alias', 'frontend-post-submission-manager'); ?></label>
         <div class="fpsm-field">
-            <input type="text" name="form_alias"/>
+            <input type="text" name="form_alias" value="<?php echo esc_attr($form_row->form_alias); ?>"/>
         </div>
     </div>
 
     <div class="fpsm-field-wrap">
         <label><?php esc_html_e('Post Status', 'frontend-post-submission-manager'); ?></label>
         <div class="fpsm-field">
-            <select name="form_details[general][post_status]">
+            <select name="form_details[basic][post_status]">
                 <?php
                 $post_statuses = $fpsm_library_obj->get_all_post_statuses();
+                $selected_post_status = (!empty($basic_settings['post_status'])) ? $basic_settings['post_status'] : 'draft';
                 foreach ($post_statuses as $post_status => $post_status_label) {
                     ?>
-                    <option value="<?php echo $post_status; ?>"><?php echo esc_attr($post_status_label); ?></option>
+                    <option value="<?php echo esc_attr($post_status); ?>" <?php selected($selected_post_status, $post_status); ?>><?php echo esc_attr($post_status_label); ?></option>
                     <?php
                 }
                 ?>
@@ -39,14 +45,18 @@
         <div class="fpsm-field-wrap">
             <label><?php esc_html_e('Post Format', 'frontend-post-submission-manager'); ?></label>
             <div class="fpsm-field">
-                <select name="form_details[general][post_format]">
+                <select name="form_details[basic][post_format]">
+                    <?php
+                    $selected_post_format = (!empty($basic_settings['post_format'])) ? $basic_settings['post_format'] : '';
+                    ?>
                     <option value=""><?php esc_html_e('Standard', 'frontend-post-submission-manager'); ?></option>
                     <?php
                     $post_formats = $fpsm_library_obj->get_registered_post_formats();
+
                     if (is_array($post_formats[0])) {
                         foreach ($post_formats[0] as $post_format) {
                             ?>
-                            <option value="<?php echo $post_format; ?>" ><?php echo ucfirst(esc_attr($post_format)); ?></option>
+                            <option value="<?php echo esc_attr($post_format); ?>" ><?php echo ucfirst(esc_attr($post_format)); ?></option>
                             <?php
                         }
                     }
--- includes/views/backend/forms/form-list.php
+++ includes/views/backend/forms/form-list.php
@@ -20,65 +20,65 @@ defined('ABSPATH') or die('No script kiddies please!!');
         </div>
         <table class="wp-list-table widefat fixed fpsm-form-lists-table">
             <thead>
-            <tr>
-                <th><?php esc_html_e('Form Title', 'frontend-post-submission-manager'); ?></th>
-                <th><?php esc_html_e('Alias', 'frontend-post-submission-manager'); ?></th>
-                <th><?php esc_html_e('Shortcode', 'frontend-post-submission-manager'); ?></th>
-                <th><?php esc_html_e('Post Type', 'frontend-post-submission-manager'); ?></th>
-                <th><?php esc_html_e('Form Type', 'frontend-post-submission-manager'); ?></th>
-                <th><?php esc_html_e('Status', 'frontend-post-submission-manager'); ?></th>
-                <th><?php esc_html_e('Action', 'frontend-post-submission-manager'); ?></th>
-            </tr>
+                <tr>
+                    <th><?php esc_html_e('Form Title', 'frontend-post-submission-manager'); ?></th>
+                    <th><?php esc_html_e('Alias', 'frontend-post-submission-manager'); ?></th>
+                    <th><?php esc_html_e('Shortcode', 'frontend-post-submission-manager'); ?></th>
+                    <th><?php esc_html_e('Post Type', 'frontend-post-submission-manager'); ?></th>
+                    <th><?php esc_html_e('Form Type', 'frontend-post-submission-manager'); ?></th>
+                    <th><?php esc_html_e('Status', 'frontend-post-submission-manager'); ?></th>
+                    <th><?php esc_html_e('Action', 'frontend-post-submission-manager'); ?></th>
+                </tr>
             </thead>
             <tbody>
-            <?php
-            global $wpdb;
-            $form_table = FPSM_FORM_TABLE;
-            $form_rows = $wpdb->get_results("select * from $form_table order by form_title asc");
-            if (!empty($form_rows)) {
-                foreach ($form_rows as $form_row) {
+                <?php
+                global $wpdb;
+                $form_table = FPSM_FORM_TABLE;
+                $form_rows = $wpdb->get_results("select * from $form_table order by form_title asc");
+                if (!empty($form_rows)) {
+                    foreach ($form_rows as $form_row) {
+                        ?>
+                        <tr>
+                            <td><a href="<?php echo admin_url('admin.php?page=fpsm&form_id=' . intval($form_row->form_id) . '&action=edit_form'); ?>"><?php echo esc_html($form_row->form_title); ?></a></td>
+                            <td><?php echo esc_html($form_row->form_alias); ?></td>
+                            <td>
+                                <span class="fpsm-shortcode-preview">[fpsm alias="subscription"]</span>
+                                <span class="fpsm-clipboard-copy"><i class="fas fa-clipboard-list"></i></span>
+                            </td>
+                            <td><?php echo esc_html($form_row->post_type); ?></td>
+                            <td><?php
+                                $form_type_label = array('login_require' => esc_html__('Login Require Form'), 'guest' => esc_html__('Guest Form', 'frontend-post-submission-manager'));
+                                echo esc_html($form_type_label[$form_row->form_type]);
+                                ?></td>
+                            <td><?php echo (!empty($form_row->form_status)) ? esc_html__('Active', 'frontend-post-submission-manager') : esc_html__('Inactive', 'frontend-post-submission-manager'); ?></td>
+                            <td>
+                                <a class="fpsm-edit" href="<?php echo admin_url('admin.php?page=fpsm&form_id=' . intval($form_row->form_id) . '&action=edit_form'); ?>" title="<?php esc_html_e('Edit Form', 'frontend-post-submission-manager'); ?>"><?php esc_html_e('Edit', 'frontend-post-submission-manager'); ?></a>
+                                <a class="fpsm-copy fpsm-form-copy" href="javascript:void(0);" data-form-id="<?php echo intval($form_row->form_id); ?>" title="<?php esc_html_e('Copy Form', 'frontend-post-submission-manager'); ?>"><?php esc_html_e('Copy', 'frontend-post-submission-manager'); ?></a>
+                                <a class="fpsm-preview" href="<?php echo site_url() . '?fpsm_preview=true&form_alias=' . esc_attr($form_row->form_alias) . '&_wpnonce=' . wp_create_nonce('std_form_preview_nonce'); ?>" target="_blank" title="<?php esc_html_e('Preview', 'frontend-post-submission-manager'); ?>"><?php esc_html_e('Preview', 'frontend-post-submission-manager'); ?></a>
+                                <a class="fpsm-delete fpsm-form-delete" href="javascript:void(0)" data-form-id="<?php echo intval($form_row->form_id); ?>" title="<?php esc_html_e('Delete Form', 'frontend-post-submission-manager'); ?>"><?php esc_html_e('Delete', 'frontend-post-submission-manager'); ?></a>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                } else {
                     ?>
                     <tr>
-                        <td><?php echo esc_html($form_row->form_title); ?></td>
-                        <td><?php echo esc_html($form_row->form_alias); ?></td>
-                        <td>
-                            <span class="fpsm-shortcode-preview">[fpsm alias="subscription"]</span>
-                            <span class="fpsm-clipboard-copy"><i class="fas fa-clipboard-list"></i></span>
-                        </td>
-                        <td><?php echo esc_html($form_row->post_type); ?></td>
-                        <td><?php
-                            $form_type_label = array('login_require' => esc_html__('Login Require Form'), 'guest' => esc_html__('Guest Form', 'frontend-post-submission-manager'));
-                            echo esc_html($form_type_label[$form_row->form_type]);
-                            ?></td>
-                        <td><?php echo (!empty($form_row->form_status)) ? esc_html__('Active', 'frontend-post-submission-manager') : esc_html__('Inactive', 'frontend-post-submission-manager'); ?></td>
-                        <td>
-                            <a class="fpsm-edit" href="<?php echo admin_url('admin.php?page=fpsm&form_id=' . intval($form_row->form_id) . '&action=edit_form'); ?>" title="<?php esc_html_e('Edit Form', 'frontend-post-submission-manager'); ?>"><?php esc_html_e('Edit', 'frontend-post-submission-manager'); ?></a>
-                            <a class="fpsm-copy fpsm-form-copy" href="javascript:void(0);" data-form-id="<?php echo intval($form_row->form_id); ?>" title="<?php esc_html_e('Copy Form', 'frontend-post-submission-manager'); ?>"><?php esc_html_e('Copy', 'frontend-post-submission-manager'); ?></a>
-                            <a class="fpsm-preview" href="<?php echo site_url() . '?fpsm_preview=true&form_alias=' . esc_attr($form_row->form_alias) . '&_wpnonce=' . wp_create_nonce('std_form_preview_nonce'); ?>" target="_blank" title="<?php esc_html_e('Preview', 'frontend-post-submission-manager'); ?>"><?php esc_html_e('Preview', 'frontend-post-submission-manager'); ?></a>
-                            <a class="fpsm-delete fpsm-form-delete" href="javascript:void(0)" data-form-id="<?php echo intval($form_row->form_id); ?>" title="<?php esc_html_e('Delete Form', 'frontend-post-submission-manager'); ?>"><?php esc_html_e('Delete', 'frontend-post-submission-manager'); ?></a>
-                        </td>
+                        <td colspan="5"><?php esc_html_e('No forms added yet.', 'frontend-post-submission-manager'); ?></td>
                     </tr>
                     <?php
                 }
-            } else {
                 ?>
-                <tr>
-                    <td colspan="5"><?php esc_html_e('No forms added yet.', 'frontend-post-submission-manager'); ?></td>
-                </tr>
-                <?php
-            }
-            ?>
             </tbody>
             <tfoot>
-            <tr>
-                <th><?php esc_html_e('Form Title', 'frontend-post-submission-manager'); ?></th>
-                <th><?php esc_html_e('Alias', 'frontend-post-submission-manager'); ?></th>
-                <th><?php esc_html_e('Shortcode', 'frontend-post-submission-manager'); ?></th>
-                <th><?php esc_html_e('Post Type', 'frontend-post-submission-manager'); ?></th>
-                <th><?php esc_html_e('Form Type', 'frontend-post-submission-manager'); ?></th>
-                <th><?php esc_html_e('Status', 'frontend-post-submission-manager'); ?></th>
-                <th><?php esc_html_e('Action', 'frontend-post-submission-manager'); ?></th>
-            </tr>
+                <tr>
+                    <th><?php esc_html_e('Form Title', 'frontend-post-submission-manager'); ?></th>
+                    <th><?php esc_html_e('Alias', 'frontend-post-submission-manager'); ?></th>
+                    <th><?php esc_html_e('Shortcode', 'frontend-post-submission-manager'); ?></th>
+                    <th><?php esc_html_e('Post Type', 'frontend-post-submission-manager'); ?></th>
+                    <th><?php esc_html_e('Form Type', 'frontend-post-submission-manager'); ?></th>
+                    <th><?php esc_html_e('Status', 'frontend-post-submission-manager'); ?></th>
+                    <th><?php esc_html_e('Action', 'frontend-post-submission-manager'); ?></th>
+                </tr>
             </tfoot>
         </table>
     </div>
