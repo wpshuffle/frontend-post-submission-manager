de07379a7c9fee7e41694f7481f6fda81f5da3a0
Regan Khadgi <regan.khadgi1@gmail.com>
2019-11-06 14:40:56 +0000

    character limit field check

--- includes/views/backend/forms/form-edit-sections/form-fields/post_content.php
+++ includes/views/backend/forms/form-edit-sections/form-fields/post_content.php
@@ -25,7 +25,7 @@
             <div class="fpsm-field-wrap">
                 <label><?php esc_html_e('Character Limit Error Message', 'frontend-post-submission-manager'); ?></label>
                 <div class="fpsm-field">
-                    <input type="text" name="form_details[form][fields][<?php echo esc_attr($field_key) ?>][character_limit_error_message]" value="<?php echo (!empty($field_details['character_limit_message'])) ? esc_attr($field_details['character_limit_message']) : ''; ?>"/>
+                    <input type="text" name="form_details[form][fields][<?php echo esc_attr($field_key) ?>][character_limit_error_message]" value="<?php echo (!empty($field_details['character_limit_error_message'])) ? esc_attr($field_details['character_limit_error_message']) : ''; ?>"/>
                 </div>
             </div>
         </div>
