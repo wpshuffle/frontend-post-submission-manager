b067fa39e2a4a668726a7fc70138b609918f954d
Regan Khadgi <regan.khadgi1@gmail.com>
2020-01-09 11:59:22 +0000

    added inline and block class for checkbox and radio button

--- includes/views/frontend/form-fields/custom-field-types/front-checkbox.php
+++ includes/views/frontend/form-fields/custom-field-types/front-checkbox.php
@@ -1,12 +1,18 @@
 <?php
-if (!empty($field_details['options'])) {
-    foreach ($field_details['options'] as $option_count => $option) {
-        ?>
-        <div class="fpsm-checkbox">
-            <input type="checkbox" name="<?php echo esc_attr($field_key); ?>" value="<?php echo esc_attr($field_details['values'][$option_count]); ?>" <?php echo (!empty($field_details['checked'][$option_count])) ? 'checked="checked"' : ''; ?>/>
-            <label for="<?php echo esc_attr($field_key); ?>"><?php echo esc_html($option); ?></label>
-        </div>
-        <?php
-    }
-}
+$display_type = $field_details['display_type'];
+$display_class = 'fpsm-checkbox-' . $display_type;
 ?>
+<div class="fpsm-checkbox-list-wrap <?php echo esc_attr($display_class); ?>">
+    <?php
+    if (!empty($field_details['options'])) {
+        foreach ($field_details['options'] as $option_count => $option) {
+            ?>
+            <div class="fpsm-checkbox">
+                <input type="checkbox" name="<?php echo esc_attr($field_key); ?>" value="<?php echo esc_attr($field_details['values'][$option_count]); ?>" <?php echo (!empty($field_details['checked'][$option_count])) ? 'checked="checked"' : ''; ?>/>
+                <label for="<?php echo esc_attr($field_key); ?>"><?php echo esc_html($option); ?></label>
+            </div>
+            <?php
+        }
+    }
+    ?>
+</div>
--- includes/views/frontend/form-fields/custom-field-types/front-radio.php
+++ includes/views/frontend/form-fields/custom-field-types/front-radio.php
@@ -1,12 +1,24 @@
 <?php
-if (!empty($field_details['options'])) {
-    foreach ($field_details['options'] as $option_count => $option) {
+$display_type = $field_details['display_type'];
+$display_class = 'fpsm-radio-' . $display_type;
+?>
+<div class="fpsm-checkbox-list-wrap <?php echo esc_attr($display_class); ?>">
+    <?php
+    if (!empty($field_details['options'])) {
         ?>
-        <div class="fpsm-radio">
-            <input type="radio" name="<?php echo esc_attr($field_key); ?>" value="<?php echo esc_attr($field_details['values'][$option_count]); ?>" <?php echo (!empty($field_details['checked'][$option_count])) ? 'checked="checked"' : ''; ?>/>
-            <label for="<?php echo esc_attr($field_key); ?>"><?php echo esc_html($option); ?></label>
-        </div>
+
+        <?php
+        foreach ($field_details['options'] as $option_count => $option) {
+            ?>
+            <div class="fpsm-radio">
+                <input type="radio" name="<?php echo esc_attr($field_key); ?>" value="<?php echo esc_attr($field_details['values'][$option_count]); ?>" <?php echo (!empty($field_details['checked'][$option_count])) ? 'checked="checked"' : ''; ?>/>
+                <label for="<?php echo esc_attr($field_key); ?>"><?php echo esc_html($option); ?></label>
+            </div>
+            <?php
+        }
+        ?>
+
         <?php
     }
-}
-?>
+    ?>
+</div>
