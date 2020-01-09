3d682f1e889a227d43ae113e07feaab294477ee7
Regan Khadgi <regan.khadgi1@gmail.com>
2020-01-09 12:43:09 +0000

    jquery datepicker added

--- includes/classes/class-fpsm-enqueue.php
+++ includes/classes/class-fpsm-enqueue.php
@@ -16,10 +16,11 @@ if (!class_exists('FPSM_Enqueue')) {
                 'no_preview' => FPSM_URL . '/assets/images/no-preview.jpg'
             );
             wp_enqueue_style('fpsm-style', FPSM_URL . '/assets/css/fpsm-frontend-style.css', array(), FPSM_VERSION);
+            wp_enqueue_style('jquery-ui', FPSM_URL . '/assets/css/jquery-ui.min.css', array(), FPSM_VERSION);
             wp_enqueue_style('fpsm-custom-style', FPSM_URL . '/assets/css/fpsm-frontend-custom-style.css', array(), FPSM_VERSION);
             wp_enqueue_style('fontawesome', FPSM_URL . '/assets/fontawesome/css/all.min.css', array(), FPSM_VERSION);
             wp_enqueue_script('fpsm-fileuploader', FPSM_URL . '/assets/js/fpsm-fileuploader.js', array(), FPSM_VERSION);
-            wp_enqueue_script('fpsm-script', FPSM_URL . '/assets/js/fpsm-frontend.js', array('jquery', 'fpsm-fileuploader', 'wp-util', 'jquery-ui-autocomplete'), FPSM_VERSION);
+            wp_enqueue_script('fpsm-script', FPSM_URL . '/assets/js/fpsm-frontend.js', array('jquery', 'fpsm-fileuploader', 'wp-util', 'jquery-ui-autocomplete', 'jquery-ui-datepicker'), FPSM_VERSION);
             wp_localize_script('fpsm-script', 'fpsm_js_obj', $js_obj);
         }
 
