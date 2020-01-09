2b3d1628482803a17bb3be21e528329963928d46
Regan Khadgi <regan.khadgi1@gmail.com>
2020-01-09 07:15:55 +0000

    frontend custom style added

--- includes/classes/class-fpsm-enqueue.php
+++ includes/classes/class-fpsm-enqueue.php
@@ -16,6 +16,7 @@ if (!class_exists('FPSM_Enqueue')) {
                 'no_preview' => FPSM_URL . '/assets/images/no-preview.jpg'
             );
             wp_enqueue_style('fpsm-style', FPSM_URL . '/assets/css/fpsm-frontend-style.css', array(), FPSM_VERSION);
+            wp_enqueue_style('fpsm-custom-style', FPSM_URL . '/assets/css/fpsm-frontend-custom-style.css', array(), FPSM_VERSION);
             wp_enqueue_style('fontawesome', FPSM_URL . '/assets/fontawesome/css/all.min.css', array(), FPSM_VERSION);
             wp_enqueue_script('fpsm-fileuploader', FPSM_URL . '/assets/js/fpsm-fileuploader.js', array(), FPSM_VERSION);
             wp_enqueue_script('fpsm-script', FPSM_URL . '/assets/js/fpsm-frontend.js', array('jquery', 'fpsm-fileuploader', 'wp-util', 'jquery-ui-autocomplete'), FPSM_VERSION);
