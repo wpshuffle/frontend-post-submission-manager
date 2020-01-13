2f87a243eac560d8220a878cb7ef4566160b21d4
Dhiran <jungrupak@gmail.com>
2020-01-13 15:55:37 +0545

    template 7 completed

--- includes/views/frontend/form-fields/custom-field-types/front-datepicker.php
+++ includes/views/frontend/form-fields/custom-field-types/front-datepicker.php
@@ -1,4 +1,3 @@
 <div class="fpsm-date-picker">
     <input type="text" class="fpsm-front-datepicker" data-date-format="<?php echo esc_attr($field_details['date_format']); ?>"/>
-    <i class="far fa-calendar-alt"></i>
 </div>
