a6235c7b0c5e550ae70e5329dc7f9efc73570869
Dhiran <jungrupak@gmail.com>
2020-01-22 16:45:41 +0545

    all 22 templates

--- includes/views/frontend/form-html.php
+++ includes/views/frontend/form-html.php
@@ -53,7 +53,7 @@ $form_template = (!empty($form_details['layout']['template'])) ? $form_details['
         }
     }
     ?>
-    <div class="fpsm-field-wrap">
+    <div class="fpsm-field-wrap fpsm-has-submit-btn">
         <div class="fpsm-field">
             <input type="submit" value="<?php echo (!empty($form_details['form']['submit_button_label'])) ? esc_attr($form_details['form']['submit_button_label']) : esc_html__('Submit', 'frontend-post-submission-manager'); ?>"/>
             <img src="<?php echo FPSM_URL . '/assets/images/ajax-loader-front.gif'; ?>" class="fpsm-ajax-loader"/>
