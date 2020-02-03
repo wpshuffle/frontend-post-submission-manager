fe4e47f575adde7d1e865d92b823307f2909e1fe
Regan Khadgi <regan.khadgi1@gmail.com>
2020-02-03 11:29:38 +0000

    login form redirect fixed

--- includes/classes/class-fpsm-shortcode.php
+++ includes/classes/class-fpsm-shortcode.php
@@ -1,31 +1,82 @@
 <?php
 
-defined( 'ABSPATH' ) or die( 'No script kiddies please!!' );
-if ( !class_exists( 'FPSM_Shortcode' ) ) {
+defined('ABSPATH') or die('No script kiddies please!!');
+if (!class_exists('FPSM_Shortcode')) {
 
     class FPSM_Shortcode {
 
         function __construct() {
-            add_shortcode( 'fpsm', array( $this, 'output_shortcode' ) );
+            add_shortcode('fpsm', array($this, 'output_shortcode'));
+            add_action('wp_login_failed', array($this, 'login_failed'));
+            add_filter('authenticate', array($this, 'verify_username_password'), 1, 3);
+            add_action('login_form', array($this, 'login_extra_fields'));
+            add_filter('login_form_middle', array($this, 'login_extra_fields'));
         }
 
-        function output_shortcode( $atts ) {
-            if ( !empty( $atts['alias'] ) ) {
+        function output_shortcode($atts) {
+            if (!empty($atts['alias'])) {
                 global $fpsm_library_obj;
                 $alias = $atts['alias'];
-                $form_row = $fpsm_library_obj->get_form_row_by_alias( $alias );
+                $form_row = $fpsm_library_obj->get_form_row_by_alias($alias);
                 // $fpsm_library_obj->print_array($form_row);
-                if ( !empty( $form_row ) ) {
-                    $form_details = maybe_unserialize( $form_row->form_details );
+                if (!empty($form_row)) {
+                    $form_details = maybe_unserialize($form_row->form_details);
                     ob_start();
                     include(FPSM_PATH . '/includes/views/frontend/form-shortcode.php');
                     $form_html = ob_get_contents();
                     ob_end_clean();
                     return $form_html;
                 } else {
-                    return esc_html__( 'Form not available for this alias.', 'frontend-post-submission-manager' );
+                    return esc_html__('Form not available for this alias.', 'frontend-post-submission-manager');
                 }
             }
+        }
+
+        function redirect_login_page() {
+            if (isset($_POST['requested_page'])) {
+                $login_page = esc_url($_POST['requested_page']);
+                $page_viewed = basename($_SERVER['REQUEST_URI']);
+
+                if ($page_viewed == "wp-login.php" && $_SERVER['REQUEST_METHOD'] == 'GET') {
+                    wp_redirect($login_page);
+                    exit;
+                }
+            }
+        }
+
+        function login_failed() {
+            if (isset($_POST['requested_page'])) {
+                $login_page = esc_url($_POST['requested_page']);
+                wp_redirect($login_page . '?login=failed');
+                exit;
+            }
+        }
+
+        function verify_username_password($user, $username, $password) {
+            if (isset($_POST['requested_page'])) {
+                $login_page = esc_url($_POST['requested_page']);
+                if ($username == "" || $password == "") {
+                    wp_redirect($login_page . "?login=empty");
+                    exit;
+                } else {
+
+                }
+            }
+        }
+
+        function login_extra_fields($login_form_buttom_html) {
+            if (!$this->is_login_page()) {
+                global $fpsm_library_obj;
+                $current_page_url = $fpsm_library_obj->get_current_page_url();
+                $login_form_html = '<input type="hidden" name="requested_page" value="' . $current_page_url . '"/>';
+                return $login_form_buttom_html . $login_form_html;
+            } else {
+                return $login_form_buttom_html;
+            }
+        }
+
+        function is_login_page() {
+            return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
         }
 
     }
--- includes/views/frontend/login-types/login-form.php
+++ includes/views/frontend/login-types/login-form.php
@@ -1,21 +1,21 @@
 <div class="fpsm-login-form-wrapper">
     <?php
-    $username_label = (!empty( $login_settings['username_label'] )) ? esc_attr( $login_settings['username_label'] ) : __( 'Username', 'frontend-post-submission-manager' );
-    $password_label = (!empty( $login_settings['password_label'] )) ? esc_attr( $login_settings['password_label'] ) : __( 'Password', 'frontend-post-submission-manager' );
-    $login_button_label = (!empty( $login_settings['login_button_label'] )) ? esc_attr( $login_settings['login_button_label'] ) : __( 'Login', 'frontend-post-submission-manager' );
-    $remember_me_label = (!empty( $login_settings['remember_me_label'] )) ? esc_attr( $login_settings['remember_me_label'] ) : __( 'Remember', 'frontend-post-submission-manager' );
-    $login_error_message = (!empty( $login_settings['login_error_message'] )) ? esc_attr( $login_settings['login_error_message'] ) : __( 'Invalid username or password.', 'frontend-post-submission-manager' );
+    $username_label = (!empty($login_settings['username_label'])) ? esc_attr($login_settings['username_label']) : __('Username', 'frontend-post-submission-manager');
+    $password_label = (!empty($login_settings['password_label'])) ? esc_attr($login_settings['password_label']) : __('Password', 'frontend-post-submission-manager');
+    $login_button_label = (!empty($login_settings['login_button_label'])) ? esc_attr($login_settings['login_button_label']) : __('Login', 'frontend-post-submission-manager');
+    $remember_me_label = (!empty($login_settings['remember_me_label'])) ? esc_attr($login_settings['remember_me_label']) : __('Remember', 'frontend-post-submission-manager');
+    $login_error_message = (!empty($login_settings['login_error_message'])) ? esc_attr($login_settings['login_error_message']) : __('Invalid username or password.', 'frontend-post-submission-manager');
     global $user_login;
-    $login = (isset( $_GET['login'] ) ) ? sanitize_text_field( $_GET['login'] ) : 0;
+    $login = (isset($_GET['login']) ) ? sanitize_text_field($_GET['login']) : 0;
 
     // In case of a login error.
-    if ( $login === "failed" ) {
-        echo '<p class="fpsm-login-msg"><strong>ERROR:</strong> ' . esc_html( $login_error_message ) . '</p>';
-    } elseif ( $login === "empty" ) {
-        echo '<p class="fpsm-login-msg"><strong>ERROR:</strong> ' . esc_html( $login_error_message ) . '</p>';
-    } elseif ( $login === "captcha_error" ) {
-        $captcha_error_message = (!empty( $fpsm_settings['captcha']['error_message'] )) ? esc_attr( $fpsm_settings['captcha']['error_message'] ) : __( 'Invalid Captcha', 'frontend-post-submission-manager' );
-        echo '<p class="fpsm-login-msg"><strong>ERROR:</strong> ' . esc_html( $captcha_error_message ) . '</p>';
+    if ($login === "failed") {
+        echo '<p class="fpsm-login-msg"><strong>ERROR:</strong> ' . esc_html($login_error_message) . '</p>';
+    } elseif ($login === "empty") {
+        echo '<p class="fpsm-login-msg"><strong>ERROR:</strong> ' . esc_html($login_error_message) . '</p>';
+    } elseif ($login === "captcha_error") {
+        $captcha_error_message = (!empty($fpsm_settings['captcha']['error_message'])) ? esc_attr($fpsm_settings['captcha']['error_message']) : __('Invalid Captcha', 'frontend-post-submission-manager');
+        echo '<p class="fpsm-login-msg"><strong>ERROR:</strong> ' . esc_html($captcha_error_message) . '</p>';
     }
     $args = array(
         'echo' => true,
@@ -35,12 +35,12 @@
     );
 
 // Calling the login form.
-    wp_login_form( $args );
+    wp_login_form($args);
     ?>
 </div>
-<script>jQuery(document).ready(function ($) {
+<!--<script>jQuery(document).ready(function ($) {
         if (window.location.href.indexOf('?') > -1) {
             history.pushState('', document.title, window.location.pathname);
         }
     });
-</script>
+</script>-->
