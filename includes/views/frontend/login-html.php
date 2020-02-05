339ff861aa8ac851033c21c679eedf1fa93228ed
Regan Khadgi <regan.khadgi1@gmail.com>
2020-02-05 10:56:14 +0000

    template name missing fixed

--- includes/views/frontend/login-html.php
+++ includes/views/frontend/login-html.php
@@ -1,8 +1,9 @@
 <?php
 
-$login_settings = (!empty( $form_details['login'] )) ? $form_details['login'] : array();
-$login_type = (!empty( $login_settings['login_type'] )) ? $login_settings['login_type'] : 'login_form';
-switch( $login_type ) {
+$login_settings = (!empty($form_details['login'])) ? $form_details['login'] : array();
+$login_type = (!empty($login_settings['login_type'])) ? $login_settings['login_type'] : 'login_form';
+$form_template = (!empty($form_details['layout']['template'])) ? $form_details['layout']['template'] : 'template-1';
+switch ($login_type) {
     case 'login_message':
         include(FPSM_PATH . '/includes/views/frontend/login-types/login-message.php');
         break;
