858c7ed94a4d63983aaf67bb5113bf729985740b
Regan Khadgi <regan.khadgi1@gmail.com>
2020-03-10 11:34:38 +0100

    file uploader intval removed

--- includes/views/frontend/form-fields/custom-field-types/front-file_uploader.php
+++ includes/views/frontend/form-fields/custom-field-types/front-file_uploader.php
@@ -14,7 +14,7 @@ $multiple_upload = (!empty($field_details['multiple_upload'])) ? 'true' : 'false
 $max_number_uploads = (!empty($field_details['max_number_uploads'])) ? $field_details['max_number_uploads'] : -1;
 $allowed_extensions = (!empty($field_details['file_extensions'])) ? $field_details['file_extensions'] : $default_allowed_extensions;
 ?>
-<div class="fpsm-file-uploader" id="fpms-file-uploader-<?php echo esc_attr($fpsm_library_obj->generate_random_string()); ?>" data-extensions="<?php echo esc_attr(implode('|', $allowed_extensions)); ?>" data-file-size-limit="<?php echo intval($upload_file_size_limit); ?>" data-label="<?php echo esc_attr($uploader_label); ?>" data-field-name="<?php echo esc_attr($field_key); ?>" data-multiple='<?php echo esc_attr($multiple_upload); ?>' data-multiple-upload-limit="<?php echo esc_attr($max_number_uploads); ?>"></div>
+<div class="fpsm-file-uploader" id="fpms-file-uploader-<?php echo esc_attr($fpsm_library_obj->generate_random_string()); ?>" data-extensions="<?php echo esc_attr(implode('|', $allowed_extensions)); ?>" data-file-size-limit="<?php echo esc_attr($upload_file_size_limit); ?>" data-label="<?php echo esc_attr($uploader_label); ?>" data-field-name="<?php echo esc_attr($field_key); ?>" data-multiple='<?php echo esc_attr($multiple_upload); ?>' data-multiple-upload-limit="<?php echo esc_attr($max_number_uploads); ?>"></div>
 <input type="hidden" class="fpsm-upload-count" value="0"/>
 <input type="hidden" name="<?php echo esc_attr($field_key); ?>" class="fpsm-media-id"/>
 <div class="fpsm-file-preview-wrap">
