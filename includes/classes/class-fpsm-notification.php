ff244499d49be0ae345dc335d3a685a21625becb
Regan Khadgi <regan.khadgi1@gmail.com>
2020-03-10 10:50:53 +0100

    post reject notification on progress

--- includes/classes/class-fpsm-notification.php
+++ includes/classes/class-fpsm-notification.php
@@ -7,6 +7,7 @@ if (!class_exists('FPSM_Notification')) {
 
         function __construct() {
             add_action('fpsm_form_submission_success', array($this, 'trigger_admin_notification'), 10, 3);
+            add_action('wp_trash_post', array($this, 'trigger_post_reject_notifications'));
         }
 
         function trigger_admin_notification($insert_update_post_id, $form_row, $action) {
@@ -14,6 +15,16 @@ if (!class_exists('FPSM_Notification')) {
                 $form_details = maybe_unserialize($form_row->form_details);
                 include(FPSM_PATH . '/includes/cores/admin-email-notification.php');
             }
+        }
+
+        function trigger_post_reject_notifications($post_id) {
+            $form_alias = get_post_meta($post_id, '_fpsm_form_alias', true);
+            if (empty($form_alias)) {
+                return;
+            }
+            global $fpsm_library_obj;
+            $form_row = $fpsm_library_obj->get_form_row_by_alias($form_alias);
+            $form_details = maybe_unserialize($form_row->form_details);
         }
 
     }
