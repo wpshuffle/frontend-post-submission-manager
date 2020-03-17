799a3871c44ff5a3b77bb2efdb6837467ef3a05f
Regan Khadgi <regan.khadgi1@gmail.com>
2020-03-17 11:41:56 +0100

    multiple select class added

--- includes/views/frontend/form-fields/taxonomy-fields/taxonomy-select.php
+++ includes/views/frontend/form-fields/taxonomy-fields/taxonomy-select.php
@@ -1,4 +1,4 @@
-<div class="fpsm-select-field">
+<div class="fpsm-select-field <?php echo (!empty($field_details['select_multiple'])) ? 'fpsm-multiple-select' : ''; ?>">
     <select name="<?php echo esc_attr($field_key) ?>" <?php if (!empty($field_details['select_multiple'])) { ?>multiple="multiple"<?php } ?>>
         <option value=""><?php echo (!empty($field_details['first_option_label'])) ? esc_html($field_details['first_option_label']) : esc_html__(sprintf('Choose %s', $taxonomy_details->label), 'frontend-post-submission-manager'); ?></option>
         <?php
