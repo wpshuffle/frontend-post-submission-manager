fa206111a616e885b0a192f1def129cd74a2e473
Regan Khadgi <regan.khadgi1@gmail.com>
2020-03-24 18:03:04 +0545

    custom media uploader bug fixed

--- assets/js/fpsm-frontend.js
+++ assets/js/fpsm-frontend.js
@@ -62,22 +62,23 @@ jQuery(document).ready(function ($) {
 
                     if (responseJSON.success) {
                         if (selector.hasClass('fpsm-custom-media-upload-button')) {
-                            switch(responseJSON.media_type){
+                            switch (responseJSON.media_type) {
                                 case 'image':
-                                    var insert_content_html = '<img class="alignnone size-full wp-image-'+responseJSON.media_id+'" src="'+responseJSON.media_full_url+'" alt="'+responseJSON.media_name+'"/>';
+                                    var insert_content_html = '<img class="alignnone size-full wp-image-' + responseJSON.media_id + '" src="' + responseJSON.media_full_url + '" alt="' + responseJSON.media_name + '"/>';
                                     break;
                                 case 'video':
-                                    var insert_content_html = '[video width="100%" height="500" '+responseJSON.media_extension+'="'+responseJSON.media_full_url+'"][/video]';
+                                    var insert_content_html = '[video width="100%" height="500" ' + responseJSON.media_extension + '="' + responseJSON.media_full_url + '"][/video]';
                                     break;
                                 case 'audio':
-                                    var insert_content_html = '[audio '+responseJSON.media_extension+'="'+responseJSON.media_full_url+'"][/audio]';
+                                    var insert_content_html = '[audio ' + responseJSON.media_extension + '="' + responseJSON.media_full_url + '"][/audio]';
                                     break;
                                 case 'others':
-                                    var insert_content_html = '<a href="'+responseJSON.media_full_url+'">'+responseJSON.media_name+'</a>';
+                                    var insert_content_html = '<a href="' + responseJSON.media_full_url + '">' + responseJSON.media_name + '</a>';
                                     break;
                             }
-                           // tinyMCE.get('fpsm_login_require_form').triggerSave();
-                            tinyMCE.get('fpsm_login_require_form').execCommand('mceInsertContent', false, insert_content_html);
+                            var tinymce_id = selector.data('tinymce-id');
+                            // tinyMCE.get('fpsm_login_require_form').triggerSave();
+                            tinyMCE.get(tinymce_id).execCommand('mceInsertContent', false, insert_content_html);
                         } else {
                             var data = {media_url: responseJSON.media_url, media_id: responseJSON.media_id, media_name: responseJSON.media_name, media_key: responseJSON.media_key}
 
--- includes/views/frontend/form-fields/front-post_content.php
+++ includes/views/frontend/form-fields/front-post_content.php
@@ -1,13 +1,13 @@
 <?php
-defined('ABSPATH') or die('No script kiddies please!!');
-$editor_type = (!empty($field_details['editor_type'])) ? $field_details['editor_type'] : 'simple';
-$post_content = (!empty($edit_post)) ? $edit_post->post_content : '';
-if ($editor_type == 'simple') {
+defined( 'ABSPATH' ) or die( 'No script kiddies please!!' );
+$editor_type = (!empty( $field_details['editor_type'] )) ? $field_details['editor_type'] : 'simple';
+$post_content = (!empty( $edit_post )) ? $edit_post->post_content : '';
+if ( $editor_type == 'simple' ) {
     ?>
-    <textarea name="<?php echo esc_attr($field_key); ?>"><?php echo $fpsm_library_obj->sanitize_html($post_content); ?></textarea>
+    <textarea name="<?php echo esc_attr( $field_key ); ?>"><?php echo $fpsm_library_obj->sanitize_html( $post_content ); ?></textarea>
     <?php
 } else {
-    switch ($editor_type) {
+    switch( $editor_type ) {
         case 'rich':
             $teeny = false;
             $show_quicktags = true;
@@ -22,19 +22,19 @@ if ($editor_type == 'simple') {
             $show_quicktags = true;
             break;
     }
-    $media_upload = (!empty($field_details['media_upload'])) ? true : false;
+    $media_upload = (!empty( $field_details['media_upload'] )) ? true : false;
     $editor_settings = array(
         'textarea_name' => $field_key,
         'media_buttons' => $media_upload,
         'teeny' => $teeny,
         'wpautop' => true,
         'quicktags' => $show_quicktags,
-        'editor_class' => apply_filters('fpsm_editor_class', 'fpsm-post-content-editor')
+        'editor_class' => apply_filters( 'fpsm_editor_class', 'fpsm-post-content-editor' )
     );
-    if ($editor_type == 'visual' || $editor_type == 'rich') {
-        if ($form_row->form_type == 'guest') {
-            if (!empty($field_details['custom_media_upload_button'])) {
-                $default_allowed_extensions = array('jpg', 'jpeg', 'png', 'gif', 'bmp', 'JPG', 'JPEG', 'PNG', 'BMP');
+    if ( $editor_type == 'visual' || $editor_type == 'rich' ) {
+        if ( $form_row->form_type == 'guest' ) {
+            if ( !empty( $field_details['custom_media_upload_button'] ) ) {
+                $default_allowed_extensions = array( 'jpg', 'jpeg', 'png', 'gif', 'bmp', 'JPG', 'JPEG', 'PNG', 'BMP' );
                 /**
                  * Filters allowed extensions for image field type
                  *
@@ -42,27 +42,28 @@ if ($editor_type == 'simple') {
                  *
                  * @since 1.0.0
                  */
-                $default_allowed_extensions = apply_filters('fpsm_image_allowed_extensions', $default_allowed_extensions);
-                $upload_file_size_limit = (!empty($field_details['upload_file_size_limit'])) ? $field_details['upload_file_size_limit'] : 5;
-                $uploader_label = (!empty($field_details['upload_button_label'])) ? $field_details['upload_button_label'] : esc_html__('Upload Image', 'frontend-post-submission-manager');
-                $multiple_upload = (!empty($field_details['multiple_upload'])) ? 'true' : 'false';
-                $max_number_uploads = (!empty($field_details['max_number_uploads'])) ? $field_details['max_number_uploads'] : -1;
-                $allowed_extensions = (!empty($field_details['file_extensions'])) ? $field_details['file_extensions'] : $default_allowed_extensions;
+                $default_allowed_extensions = apply_filters( 'fpsm_image_allowed_extensions', $default_allowed_extensions );
+                $upload_file_size_limit = (!empty( $field_details['upload_file_size_limit'] )) ? $field_details['upload_file_size_limit'] : 5;
+                $uploader_label = (!empty( $field_details['upload_button_label'] )) ? $field_details['upload_button_label'] : esc_html__( 'Upload Image', 'frontend-post-submission-manager' );
+                $multiple_upload = (!empty( $field_details['multiple_upload'] )) ? 'true' : 'false';
+                $max_number_uploads = (!empty( $field_details['max_number_uploads'] )) ? $field_details['max_number_uploads'] : -1;
+                $allowed_extensions = (!empty( $field_details['file_extensions'] )) ? $field_details['file_extensions'] : $default_allowed_extensions;
                 ?>
                 <div class="fpsm-file-uploader fpsm-custom-media-upload-button"
-                     id="fpsm-file-uploader-<?php $fpsm_library_obj->generate_random_string(); ?>"
-                     data-extensions="<?php echo esc_attr(implode('|', $allowed_extensions)); ?>"
-                     data-file-size-limit="<?php echo esc_attr($upload_file_size_limit); ?>"
-                     data-label="<?php echo esc_attr($uploader_label); ?>"
-                     data-field-name="<?php echo esc_attr($field_key); ?>"
+                     id="fpsm-file-uploader-<?php echo esc_attr( $fpsm_library_obj->generate_random_string() ); ?>"
+                     data-extensions="<?php echo esc_attr( implode( '|', $allowed_extensions ) ); ?>"
+                     data-file-size-limit="<?php echo esc_attr( $upload_file_size_limit ); ?>"
+                     data-label="<?php echo esc_attr( $uploader_label ); ?>"
+                     data-field-name="<?php echo esc_attr( $field_key ); ?>"
                      data-multiple='true'
-                     data-multiple-upload-limit="-1">
+                     data-multiple-upload-limit="-1"
+                     data-tinymce-id="<?php echo esc_attr( 'fpsm_' . $form_row->form_alias ); ?>">
                 </div>
                 <?php
             }
         } else {
-            if ((!current_user_can('upload_files') || empty($field_details['media_upload'])) && !empty($field_details['custom_media_upload_button'])) {
-                $default_allowed_extensions = array('jpg', 'jpeg', 'png', 'gif', 'bmp', 'JPG', 'JPEG', 'PNG', 'BMP');
+            if ( (!current_user_can( 'upload_files' ) || empty( $field_details['media_upload'] )) && !empty( $field_details['custom_media_upload_button'] ) ) {
+                $default_allowed_extensions = array( 'jpg', 'jpeg', 'png', 'gif', 'bmp', 'JPG', 'JPEG', 'PNG', 'BMP' );
                 /**
                  * Filters allowed extensions for image field type
                  *
@@ -70,21 +71,22 @@ if ($editor_type == 'simple') {
                  *
                  * @since 1.0.0
                  */
-                $default_allowed_extensions = apply_filters('fpsm_image_allowed_extensions', $default_allowed_extensions);
-                $upload_file_size_limit = (!empty($field_details['upload_file_size_limit'])) ? $field_details['upload_file_size_limit'] : 5;
-                $uploader_label = (!empty($field_details['upload_button_label'])) ? $field_details['upload_button_label'] : esc_html__('Upload Image', 'frontend-post-submission-manager');
-                $multiple_upload = (!empty($field_details['multiple_upload'])) ? 'true' : 'false';
-                $max_number_uploads = (!empty($field_details['max_number_uploads'])) ? $field_details['max_number_uploads'] : -1;
-                $allowed_extensions = (!empty($field_details['file_extensions'])) ? $field_details['file_extensions'] : $default_allowed_extensions;
+                $default_allowed_extensions = apply_filters( 'fpsm_image_allowed_extensions', $default_allowed_extensions );
+                $upload_file_size_limit = (!empty( $field_details['upload_file_size_limit'] )) ? $field_details['upload_file_size_limit'] : 5;
+                $uploader_label = (!empty( $field_details['upload_button_label'] )) ? $field_details['upload_button_label'] : esc_html__( 'Upload Image', 'frontend-post-submission-manager' );
+                $multiple_upload = (!empty( $field_details['multiple_upload'] )) ? 'true' : 'false';
+                $max_number_uploads = (!empty( $field_details['max_number_uploads'] )) ? $field_details['max_number_uploads'] : -1;
+                $allowed_extensions = (!empty( $field_details['file_extensions'] )) ? $field_details['file_extensions'] : $default_allowed_extensions;
                 ?>
                 <div class="fpsm-file-uploader fpsm-custom-media-upload-button"
-                     id="fpsm-file-uploader-<?php $fpsm_library_obj->generate_random_string(); ?>"
-                     data-extensions="<?php echo esc_attr(implode('|', $allowed_extensions)); ?>"
-                     data-file-size-limit="<?php echo esc_attr($upload_file_size_limit); ?>"
-                     data-label="<?php echo esc_attr($uploader_label); ?>"
-                     data-field-name="<?php echo esc_attr($field_key); ?>"
+                     id="fpsm-file-uploader-<?php echo esc_attr( $fpsm_library_obj->generate_random_string() ); ?>"
+                     data-extensions="<?php echo esc_attr( implode( '|', $allowed_extensions ) ); ?>"
+                     data-file-size-limit="<?php echo esc_attr( $upload_file_size_limit ); ?>"
+                     data-label="<?php echo esc_attr( $uploader_label ); ?>"
+                     data-field-name="<?php echo esc_attr( $field_key ); ?>"
                      data-multiple='true'
-                     data-multiple-upload-limit="-1">
+                     data-multiple-upload-limit="-1"
+                     data-tinymce-id="<?php echo esc_attr( 'fpsm_' . $form_row->form_alias ); ?>">
                 </div>
                 <?php
             }
@@ -92,6 +94,6 @@ if ($editor_type == 'simple') {
     }
     ?>
     <?php
-    wp_editor($post_content, 'fpsm_' . $form_row->form_alias, $editor_settings);
+    wp_editor( $post_content, 'fpsm_' . $form_row->form_alias, $editor_settings );
 }
 ?>
