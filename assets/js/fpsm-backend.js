f4b82541b4df2b4d50aa1f597efab882c301f54e
Regan Khadgi <regan.khadgi1@gmail.com>
2020-03-28 13:32:09 +0545

    label background and color added

--- assets/js/fpsm-backend.js
+++ assets/js/fpsm-backend.js
@@ -488,6 +488,12 @@ jQuery(document).ready(function ($) {
         var template = $(this).val();
         $('.fpsm-form-template-preview-img').hide();
         $('.fpsm-form-template-preview-img[data-template-id="' + template + '"]').show();
+        if (template == 'template-7' || template == 'template-12' || template == 'template-22') {
+            $('.fpsm-label-background-ref').show();
+        } else {
+            $('.fpsm-label-background-ref').hide();
+
+        }
     });
 
     $('.fpsm-color-picker').wpColorPicker();
@@ -518,6 +524,8 @@ jQuery(document).ready(function ($) {
                     selector.parent().find('.fpsm-media-preview').html('<img src="' + uploaded_image.toJSON().sizes.thumbnail.url + '"/>');
                 });
     });
+
+
 
 
 });
