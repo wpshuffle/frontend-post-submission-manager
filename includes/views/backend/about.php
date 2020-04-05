d27ebcf19f101f1d64abac3831fdba742f329c13
Regan Khadgi <regan.khadgi1@gmail.com>
2020-04-05 23:36:52 +0545

    about section added

---
+++ includes/views/backend/about.php
@@ -0,0 +1,60 @@
+<div class="wrap fpsm-wrap">
+    <div class="fpsm-header fpsm-clearfix">
+        <h1 class="fpsm-floatLeft"><?php esc_html_e( 'Frontend Post Submission Manager', 'frontend-post-submission-manager' ); ?></h1>
+        <div class="fpsm-add-wrap">
+            <a href="<?php echo admin_url( 'admin.php?page=fpsm-add-new-form' ); ?>"><input type="button" class="fpsm-button-primary" value="<?php echo esc_html_e( 'Add New Form', 'frontend-post-submission-manager' ); ?>"></a>
+        </div>
+    </div>
+
+    <div class="fpsm-block-wrap">
+        <div class="fpsm-content-block">
+            <h2><?php esc_html_e( 'About Plugin', 'frontend-post-submission-manager' ); ?></h2>
+            <p>As the name explains, <strong>Frontend Post Submission Manager</strong> is a premium WordPress plugin which facilitates with the functionality to receive Posts or Any other Post Types from frontend with or without logging in along with Frontend Post Management Dashboard.</p>
+            <p>You can create unlimited forms with <strong>drag and drop form builder</strong>, add unlimited <strong>custom fields</strong> with various field types support such as <strong>Textfield, Textarea, Radio Button, Checkbox, Datepicker, Fileuploader</strong> and many more.</p>
+        </div>
+        <div class="fpsm-content-block">
+            <h2><?php esc_html_e( 'Features', 'frontend-post-submission-manager' ); ?></h2>
+            <ul class="fpsm-bullets">
+                <li><?php esc_html_e( 'Unlimited Forms with Drag and Drop form Builder', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Default Post Types and Custom Post Types Support', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Default Taxonomies and Custom Taxonomies Support', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Unlimited Custom Fields with various Field Types Support', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Custom Fields Frontend Display Support', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Custom Fields Display in Backend too', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( '20+ Beautiful Pre Designed templates', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Frontend Post Management Dashboard', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Guest Post Support', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Secure Form Submission with Google Captcha Integrated ', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Admin and User Notification Configurations', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Layout Customization Options', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Frontend Form Preview', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Redirection option available after Successful submission', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Image and other files direct insertion into post content', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Configure Submited Post Status', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Configure Post Author', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Configure Post Format', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Character Limit Configuration', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Enable/disable Form Components', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'All device friendly and browser Compatibility', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Dedicated Support', 'frontend-post-submission-manager' ); ?></li>
+                <li><?php esc_html_e( 'Translation Ready', 'frontend-post-submission-manager' ); ?></li>
+
+            </ul>
+        </div>
+        <div class="fpsm-content-block">
+            <h2><?php esc_html_e( 'About WP Shuffle', 'frontend-post-submission-manager' ); ?></h2>
+            <p><?php esc_html_e( 'We are a bunch of WordPress Enthusiasts with an aim to develop the WordPress Themes and Plugins that adds a value to any WordPress site. Our mission is to create a WordPress product that is easy to use, highly customizable and offers innovative features that are useful for every another WordPress site.', 'frontend-post-submission-manager' ); ?></p>
+            <p><?php esc_html_e( 'If talking about support, we value our customers more that we value our products so our qualified support team is always there to provide any assistance with our products.', 'frontend-post-submission-manager' ); ?></p>
+        </div>
+        <div class="fpsm-content-block">
+            <h2><?php esc_html_e( 'Our Plugins', 'frontend-post-submission-manager' ); ?></h2>
+            <a href="https://wpshuffle.com/wordpress-plugins">https://wpshuffle.com/wordpress-plugins/</a>
+        </div>
+        <div class="fpsm-content-block">
+            <h2><?php esc_html_e( 'Our Themes', 'frontend-post-submission-manager' ); ?></h2>
+            <a href="https://wpshuffle.com/wordpress-themes">https://wpshuffle.com/wordpress-themes/</a>
+        </div>
+
+
+    </div>
+</div>
