d63123dd01e39645150349bec5064a475251abbd
Regan Khadgi <regan.khadgi1@gmail.com>
2020-04-24 23:47:07 +0545

    preview title translation ready

--- includes/views/frontend/frontend-preview-template.php
+++ includes/views/frontend/frontend-preview-template.php
@@ -1,5 +1,5 @@
 <?php
-defined('ABSPATH') or die('No script kiddies please!!');
+defined( 'ABSPATH' ) or die( 'No script kiddies please!!' );
 /**
  * Frontend Post Submission Preview Template File
  *
@@ -7,14 +7,14 @@ defined('ABSPATH') or die('No script kiddies please!!');
  */
 get_header();
 ?>
-    <div class="fpsm-page-title">
-        <h2>Template Preview</h2>
-    </div>
-<div class="fpsm-preview-note"><?php esc_html_e('Please note that this preview may differ when used in frontend as per your active theme\'s layout.', 'frontend-post-submission-manager'); ?></div>
+<div class="fpsm-page-title">
+    <h2><?php esc_html_e( 'Form Preview', 'frontend-post-submission-manager' ); ?></h2>
+</div>
+<div class="fpsm-preview-note"><?php esc_html_e( 'Please note that this preview may differ when used in frontend as per your active theme\'s layout.', 'frontend-post-submission-manager' ); ?></div>
 <div id="fpsm-preview-wrap">
     <?php
-    $form_alias = sanitize_text_field($_GET['fpsm_form_alias']);
-    echo do_shortcode('[fpsm alias="' . $form_alias . '"]');
+    $form_alias = sanitize_text_field( $_GET['fpsm_form_alias'] );
+    echo do_shortcode( '[fpsm alias="' . $form_alias . '"]' );
     ?>
 </div>
 <?php
