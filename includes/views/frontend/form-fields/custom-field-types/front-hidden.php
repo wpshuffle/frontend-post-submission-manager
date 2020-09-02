3832ca2e9da7a4ef356a0c3aaba2ce07f1edc09e
Regan Khadgi <regan.khadgi1@gmail.com>
2020-09-02 11:01:29 +0545

    file download on progress

--- includes/views/frontend/form-fields/custom-field-types/front-hidden.php
+++ includes/views/frontend/form-fields/custom-field-types/front-hidden.php
@@ -6,20 +6,21 @@ switch( $hidden_field_type ) {
         $hidden_value = $fpsm_library_obj->get_current_page_url();
         break;
     case 'user_ip':
-        $hidden_value = $fpsm_library_obj->get_public_ip();
+        $hidden_value = $fpsm_library_obj->get_user_IP();
         break;
     case 'request':
         if ( !empty( $field_details['url_parameter_key'] ) ) {
             $hidden_value = sanitize_text_field( $_REQUEST[$field_details['url_parameter_key']] );
         } else {
-
+            $hidden_value = '';
         }
         break;
     case 'prefilled':
+        $hidden_value = $field_details['pre_filled_value'];
         break;
 }
 $custom_field_array = explode( '|', $field_key );
 $custom_field_meta_key = end( $custom_field_array );
-$custom_field_saved_value = (!empty( $edit_post )) ? get_post_meta( $post_id, $custom_field_meta_key, true ) : '';
+$custom_field_saved_value = (!empty( $edit_post )) ? get_post_meta( $post_id, $custom_field_meta_key, true ) : $hidden_value;
 ?>
 <input type="hidden" name="<?php echo esc_attr( $field_key ); ?>" value="<?php echo esc_attr( $custom_field_saved_value ); ?>"/>
