d903972de34c6cf9a4d8a319b001fb7a4895a148
Regan Khadgi <regan.khadgi1@gmail.com>
2020-10-06 14:21:27 +0545

    updated version to 1.2.0

--- includes/classes/class-frontend-post-submission-manager.php
+++ includes/classes/class-frontend-post-submission-manager.php
@@ -15,7 +15,7 @@ if ( !class_exists( 'Frontend_Post_Submission_Manager' ) ) {
          *
          * @var string
          */
-        public $version = '1.1.1';
+        public $version = '1.2.0';
 
         /**
          * The single instance of the class.
