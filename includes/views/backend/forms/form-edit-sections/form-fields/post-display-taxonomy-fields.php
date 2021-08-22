a694da69adf9ab9542fd93b19721ed8c053d9327
Regan Khadgi <regan.khadgi1@gmail.com>
2021-08-22 16:46:39 +0545

    taxonomy display as link toggle fix

--- includes/views/backend/forms/form-edit-sections/form-fields/post-display-taxonomy-fields.php
+++ includes/views/backend/forms/form-edit-sections/form-fields/post-display-taxonomy-fields.php
@@ -1,11 +1,11 @@
 <div class="fpsm-field-wrap">
     <label><?php esc_html_e('Display as Link', 'frontend-post-submission-manager'); ?></label>
     <div class="fpsm-field">
-        <input type="checkbox" name="<?php echo esc_attr($field_name_prefix) ?>[display_as_link]" value="1" <?php echo (!empty($field_details['display_as_link'])) ? 'checked="checked"' : ''; ?> class="fpsm-checkbox-toggle-trigger" data-toggle-class="fpsm-display-link-ref" />
+        <input type="checkbox" name="<?php echo esc_attr($field_name_prefix) ?>[display_as_link]" value="1" <?php echo (!empty($field_details['display_as_link'])) ? 'checked="checked"' : ''; ?> class="fpsm-checkbox-toggle-trigger" data-toggle-class="fpsm-display-link-ref-<?php echo esc_attr($taxonomy); ?>" />
         <p class="description"><?php esc_html_e('Please check if you want to display the terms/categories as link in the post detail template.', 'frontend-post-submission-manager'); ?></p>
     </div>
 </div>
-<div class="fpsm-field-wrap fpsm-display-link-ref <?php echo (empty($field_details['display_as_link'])) ? 'fpsm-display-none' : ''; ?>">
+<div class="fpsm-field-wrap fpsm-display-link-ref-<?php echo esc_attr($taxonomy); ?> <?php echo (empty($field_details['display_as_link'])) ? 'fpsm-display-none' : ''; ?>">
     <label><?php esc_html_e('Open in new tab', 'frontend-post-submission-manager'); ?></label>
     <div class="fpsm-field">
         <input type="checkbox" name="<?php echo esc_attr($field_name_prefix) ?>[open_in_new_tab]" value="1" <?php echo (!empty($field_details['open_in_new_tab'])) ? 'checked="checked"' : ''; ?> />
