1b652990bd0bc56bd1caebb2bde5665de78b3832
Regan Khadgi <regan.khadgi1@gmail.com>
2023-03-08 12:53:57 +0545

    changed the version number

--- includes/classes/class-frontend-post-submission-manager.php
+++ includes/classes/class-frontend-post-submission-manager.php
@@ -15,7 +15,7 @@ if (!class_exists('Frontend_Post_Submission_Manager')) {
          *
          * @var string
          */
-        public $version = '1.3.8';
+        public $version = '1.3.9';
 
         /**
          * The single instance of the class.
