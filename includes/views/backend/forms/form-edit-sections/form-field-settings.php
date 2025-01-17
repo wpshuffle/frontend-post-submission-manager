0a119277f3c0c3000adc7830f4f882d147fe1e7c
Regan Khadgi <regan.khadgi1@gmail.com>
2025-01-17 14:44:53 +0545

    fix: Fallback default fields append

--- includes/views/backend/forms/form-edit-sections/form-field-settings.php
+++ includes/views/backend/forms/form-edit-sections/form-field-settings.php
@@ -1,5 +1,6 @@
 <?php
 $form_settings = (!empty($form_details['form'])) ? $form_details['form'] : array();
+$default_fields = $fpsm_library_obj->get_default_fields($form_row->post_type,$form_row->form_type);
 ?>
 <div class="fpsm-settings-each-section fpsm-display-none fpsm-clearfix" data-tab="form">
     <div class="fpsm-form-fields-wrap ">
@@ -12,6 +13,7 @@ $form_settings = (!empty($form_details['form'])) ? $form_details['form'] : array
                 $post_type = $form_row->post_type;
                 $form_type = $form_row->form_type;
                 $form_fields = (!empty($form_settings['fields'])) ? $form_settings['fields'] : $fpsm_library_obj->get_default_fields($post_type, $form_type);
+                $form_fields = array_merge($default_fields,$form_fields);
                 foreach ($form_fields as $field_key => $field_details) {
                     $field_file = $fpsm_library_obj->generate_field_file($field_key);
                     if (file_exists(FPSM_PATH . '/includes/views/backend/forms/form-edit-sections/form-fields/' . $field_file)) {
