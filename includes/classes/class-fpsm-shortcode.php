32ef8b0151d529452c6700215a98977754936ee0
Ashish Shahi <aacescshahi@gmail.com>
2026-01-05 15:43:13 +0545

    preview image refined

--- includes/classes/class-fpsm-shortcode.php
+++ includes/classes/class-fpsm-shortcode.php
@@ -42,7 +42,9 @@ if (!class_exists('FPSM_Shortcode')) {
                 wp_enqueue_style('fpsm-login-style', FPSM_URL . '/assets/css/fpsm-login-form-style.css', array(), FPSM_VERSION);
             }
             wp_enqueue_style('fpsm-fonts', FPSM_URL . '/assets/font-face/NunitoSans/stylesheet.css', array(), FPSM_VERSION);
-            wp_enqueue_style('fpsm-fonts', FPSM_URL . '/assets/font-face/comingsoon/stylesheet.css', array(), FPSM_VERSION);
+            wp_enqueue_style('fpsm-fonts-1', FPSM_URL . '/assets/font-face/comingsoon/stylesheet.css', array(), FPSM_VERSION);
+            wp_enqueue_style('fpsm-fonts-3', FPSM_URL . '/assets/font-face/lobstertwo/stylesheet.css', array(), FPSM_VERSION);
+            wp_enqueue_style('fpsm-fonts-4', FPSM_URL . '/assets/font-face/playlist/stylesheet.css', array(), FPSM_VERSION);
             if (empty($fpsm_settings['disable_jquery_ui_css'])) {
                 wp_enqueue_style('jquery-ui', FPSM_URL . '/assets/css/jquery-ui.min.css', array(), FPSM_VERSION);
             }
